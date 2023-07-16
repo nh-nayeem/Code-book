6bdc3fed250be1027aed05426a2f2ac5c6b49753
nh-nayeem <95094210+nh-nayeem@users.noreply.github.com>
2023-07-16 15:10:53 +0600

    Delete Extended GCD.cpp

--- Math/Number Theory/Extended GCD.cpp
+++
@@ -1,8 +0,0 @@
-struct GCD_type { long long x, y, d; };
-GCD_type ex_GCD(long long a, long long b)
-{
-    if (b == 0) return {1, 0, a};
-    GCD_type pom = ex_GCD(b, a % b);
-    return {pom.y, pom.x - a / b * pom.y, pom.d};
-}
-// modinv(a, mod) = (x % mod + mod) % mod, x = ex_GCD(a, mod) -> x
