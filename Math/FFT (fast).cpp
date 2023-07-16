e85d4409d009a1b879315977ec8d6f44ec6b8b6d
nh-nayeem <95094210+nh-nayeem@users.noreply.github.com>
2023-07-16 11:56:24 +0600

    Update FFT (fast).cpp

--- Math/FFT (fast).cpp
+++ Math/FFT (fast).cpp
@@ -1,81 +1,77 @@
-//Complex Class Hard Coded
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9;
+
+const double PI = acos(-1);
 struct base {
-    typedef double T;
-    T re, im;
-    base() :re(0), im(0) {}
-    base(T re) :re(re), im(0) {}
-    base(T re, T im) :re(re), im(im) {}
-    base op + (const base& o) const {
-        return base(re + o.re, im + o.im);
-    }
-    base op - (const base& o) const {
-        return base(re - o.re, im - o.im);
-    }
-    base op * (const base& o) const {
-        return base(re * o.re - im * o.im, re * o.im + im * o.re);
-    }
-    base op * (ld k) const {return base(re * k, im * k) ;}
-    base conj() const { return base(re, -im);}
+  double a, b;
+  base(double a = 0, double b = 0) : a(a), b(b) {}
+  const base operator + (const base &c) const
+    { return base(a + c.a, b + c.b); }
+  const base operator - (const base &c) const
+    { return base(a - c.a, b - c.b); }
+  const base operator * (const base &c) const
+    { return base(a * c.a - b * c.b, a * c.b + b * c.a); }
 };
-const ll N = 21;
-const ll MAXN = (1<<N);
-base w[MAXN];
-base f1[MAXN];
-ll rev[MAXN];
-void build_rev(ll k) {
-    static ll rk = -1;
-    if( k == rk )return ;
-    rk = k;
-    for(ll i=1; i<=(1<<k); i++) {
-        ll j = rev[i-1], t = k-1;
-        while( t >= 0 && ((j>>t)&1) ) {j ^= 1 << t;--t;}
-        if( t >= 0 ) { j ^= 1 << t; --t;}
-        rev[i] = j;
+void fft(vector<base> &p, bool inv = 0) {
+  int n = p.size(), i = 0;
+  for(int j = 1; j < n - 1; ++j) {
+    for(int k = n >> 1; k > (i ^= k); k >>= 1);
+    if(j < i) swap(p[i], p[j]);
+  }
+  for(int l = 1, m; (m = l << 1) <= n; l <<= 1) {
+    double ang = 2 * PI / m;
+    base wn = base(cos(ang), (inv ? 1. : -1.) * sin(ang)), w;
+    for(int i = 0, j, k; i < n; i += m) {
+      for(w = base(1, 0), j = i, k = i + l; j < k; ++j, w = w * wn) {
+        base t = w * p[j + l];
+        p[j + l] = p[j] - t;
+        p[j] = p[j] + t;
+      }
     }
+  }
+  if(inv) for(int i = 0; i < n; ++i) p[i].a /= n, p[i].b /= n;
 }
-void fft(base *a, ll k) {
-    build_rev(k);
-    ll n = 1 << k;
-    for(ll i=0; i<n; i++) if( rev[i] > i ) swap(a[i], a[rev[i]]);
-    for(ll l = 2, llo = 1; l <= n; l += l, llo += llo) {
-        if( w[llo].re == 0 && w[llo].im == 0 ) {
-            ld angle = M_PI / llo;
-            base ww( cosl(angle), sinl(angle) );
-            if( llo > 1 ) for(ll j = 0; j < llo; ++j) {
-                    if( j & 1 ) w[llo + j] = w[(llo+j)/2] * ww;
-                    else w[llo + j] = w[(llo+j)/2];
-                }
-            else w[llo] = base(1, 0);
-        }
-        for(ll i = 0; i < n; i += l) {
-            for(ll j=0; j<llo; j++) {
-                base v = a[i + j], u = a[i + j + llo] * w[llo + j];
-                a[i + j] = v + u;
-                a[i + j + llo] = v - u;
-            }
-        }
-    }
+vector<long long> multiply(vector<int> &a, vector<int> &b) {
+  int n = a.size(), m = b.size(), t = n + m - 1, sz = 1;
+  while(sz < t) sz <<= 1;
+  vector<base> x(sz), y(sz), z(sz);
+  for(int i = 0 ; i < sz; ++i) {
+    x[i] = i < (int)a.size() ? base(a[i], 0) : base(0, 0);
+    y[i] = i < (int)b.size() ? base(b[i], 0) : base(0, 0);
+  }
+  fft(x), fft(y);
+  for(int i = 0; i < sz; ++i) z[i] = x[i] * y[i];
+  fft(z, 1);
+  vector<long long> ret(sz);
+  for(int i = 0; i < sz; ++i) ret[i] = (long long) (z[i].a + 0.5);
+  while((int)ret.size() > 1 && ret.back() == 0) ret.pop_back();
+  return ret;
 }
-void Multiply(vector<ll>& a, vector<ll>& b,vector<ll>& res) {
-    ll k = 1;
-    while( (1<<k) < (a.size() + b.size()) ) ++k;
-    ll n = (1<<k);
-    for(ll i=0; i<n; i++) f1[i] = base(0,0);
-    for(ll i=0; i<a.size(); i++) f1[i] = f1[i] + base(a[i], 0);
-    for(ll i=0; i<b.size(); i++) f1[i] = f1[i] + base(0, b[i]);
-    fft(f1, k);
-    for(ll i=0; i<1+n/2; i++) {
-        base p = f1[i] + f1[(n-i)%n].conj();
-        base _q = f1[(n-i)%n] - f1[i].conj();
-        base q(_q.im, _q.re);
-        f1[i] = (p * q) * 0.25;
-        if( i > 0 ) f1[(n - i)] = f1[i].conj();
-    }
-    for(ll i=0; i<n; i++) f1[i] = f1[i].conj();
-    fft(f1, k);
-    res.resize(a.size() + b.size());
-    for(ll i=0; i<res.size(); i++) {
-        if(fabs(f1[i].re) < eps) res[i]=0;
-        else res[i] = f1[i].re / fabs(f1[i].re) * ll (abs(f1[i].re / n) + 0.5);
-    }
+
+long long ans[N];
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n, x; cin >> n >> x;
+  vector<int> a(n + 1, 0), b(n + 1, 0), c(n + 1, 0);
+  int nw = 0;
+  a[0]++; b[n]++;
+  long long z = 0;
+  for (int i = 1; i <= n; i++) {
+    int k; cin >> k;
+    nw += k < x;
+    a[nw]++; b[-nw + n]++;
+    z += c[nw] + !nw; c[nw]++;
+  }
+  auto res = multiply(a, b);
+  for (int i = n + 1; i < res.size(); i++) {
+    ans[i - n] += res[i];
+  }
+  ans[0] = z;
+  for (int i = 0; i <= n; i++) cout << ans[i] << ' ';
+  cout << '\n';
+  return 0;
 }
+//https://codeforces.com/contest/993/problem/E
