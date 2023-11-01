aa83810956fd6a2efb1a6cd03ca2929bfa2d3b45
nh-nayeem <95094210+nh-nayeem@users.noreply.github.com>
2023-11-01 17:25:27 +0600

    Update Aho Corasick.cpp

--- String/Aho Corasick.cpp
+++ String/Aho Corasick.cpp
@@ -1,100 +1,82 @@
-// Aho-Corasick
-// Complexity : |Text| + Sum of all |Pattern| + O(number of Occurrences)
-// if occurrence positions needed, Worst Case Complexity : (SumLen) Root (SumLen)
-#include <bits/stdc++.h>
-using namespace std;
-const int MAXT = 1000005;  // Length of Text
-const int MAXP = 1000005;  // Sun of all |Pattern|
-const int MAXQ = 1000005;  // Number of Patterns
+const int N = 1e5 + 9;
 
-int n;
-map<char,int> Next[MAXP];
-int Root;                 // AC automaton Root
-int Nnode;                // Total node count
-int Link[MAXP];           // failure links
-int Len[MAXP];            // Len[i] = length of i-th pattern
-vector<int> End[MAXP];    // End[i] = indices of patterns those end in node i
-// vector<int> Occ[MAX];  // Occ[i] = occurrences of i-th pattern
-vector<int> edgeLink[MAXP];
-vector<int> perNodeText[MAXP];
-
-int in[MAXQ], out[MAXQ];
-int euler[MAXT];
-int Time;
-
-void Clear(int node){
-    Next[node].clear();
-    End[node].clear();
-    edgeLink[node].clear();
-    perNodeText[node].clear();
-}
-
-void init(){
-    Time = 0;
-    Root = Nnode = 0;
-    Clear(Root);
-}
-
-void insertword(string p,int ind){
-    int len = p.size();
-    int now = Root;
-    for(int i=0; i<len; i++){
-        if(!Next[now][p[i]]){
-            Next[now][p[i]] = ++Nnode;
-            Clear(Nnode);
+//credit: Alpha_Q
+struct AC {
+  int N, P;
+  const int A = 26;
+  vector <vector <int>> next;
+  vector <int> link, out_link;
+  vector <vector <int>> out;
+  AC(): N(0), P(0) {node();}
+  int node() {
+    next.emplace_back(A, 0);
+    link.emplace_back(0);
+    out_link.emplace_back(0);
+    out.emplace_back(0);
+    return N++;
+  }
+  inline int get (char c) {
+    return c - 'a';
+  }
+  int add_pattern (const string T) {
+    int u = 0;
+    for (auto c : T) {
+      if (!next[u][get(c)]) next[u][get(c)] = node();
+      u = next[u][get(c)];
+    }
+    out[u].push_back(P);
+    return P++;
+  }
+  void compute() {
+    queue <int> q;
+    for (q.push(0); !q.empty();) {
+      int u = q.front(); q.pop();
+      for (int c = 0; c < A; ++c) {
+        int v = next[u][c];
+        if (!v) next[u][c] = next[link[u]][c];
+        else {
+          link[v] = u ? next[link[u]][c] : 0;
+          out_link[v] = out[link[v]].empty() ? out_link[link[v]] : link[v];
+          q.push(v);
         }
-        now = Next[now][p[i]];
+      }
     }
-    End[now].push_back(ind);
-}
+  }
+  int advance (int u, char c) {
+    while (u && !next[u][get(c)]) u = link[u];
+    u = next[u][get(c)];
+    return u;
+  }
+};
 
-void push_links(){
-    queue<int> q;
-    Link[0] = -1;
-    q.push(0);
-    while(!q.empty()){
-        int u = q.front();
-        q.pop();
-        for(auto edge : Next[u]){
-            char ch = edge.first;
-            int v = edge.second;
-            int j = Link[u];
-
-            while(j != -1 && !Next[j][ch]) j = Link[j];
-            if(j != -1) Link[v] = Next[j][ch];
-            else Link[v] = 0;
-
-            q.push(v);
-            edgeLink[Link[v]].push_back(v);
-            // for(int x : End[Link[v]]) End[v].push_back(x);
+int32_t main() {
+  int t, cs = 0; cin >> t;
+  while (t--) {
+    int n; cin >> n;
+    vector<string> v(n);
+    for (auto &i: v) cin >> i;
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    AC aho;
+    vector<int> len(n + 3, 0);
+    for (auto s: v) {
+      len[aho.add_pattern(s)] = s.size();
+    }
+    aho.compute();
+    string s; cin >> s;
+    n = s.size();
+    vector<int> dp(n, n + 10);
+    int u = 0;
+    for (int i = 0; i < n; i++) {
+      char c = s[i];
+      u = aho.advance(u, c);
+      for (int v = u; v; v = aho.out_link[v]) {
+        for (auto p : aho.out[v]) { 
+          dp[i] = min(dp[i], (i - len[p] >= 0 ? dp[i - len[p]] : 0) + 1);
         }
+      }
     }
-}
-
-void traverse(string s){
-    int len = s.size();
-    int now = Root;
-    for(int i = 0; i < len; i++)    {
-        while(now != -1 && !Next[now][s[i]]) now = Link[now];
-        if(now!=-1) now = Next[now][s[i]];
-        else now = 0;
-        perNodeText[now].push_back(i+1);  // using 1 based indexing for text indices
-        // for(int x=0;x<End[now].size();x++) Occ[End[now][x]].push_back(i);
-    }
-}
-
-// After dfs, the occurence of ith query string will be the count of
-// all the occurrence of the subtree under the endNode of ith string
-void dfs(int pos){
-    for(int q : End[pos]) in[q] = Time + 1;
-    for(int val : perNodeText[pos]) euler[++Time] = val;
-    for(int to : edgeLink[pos]) dfs(to);
-    for(int q : End[pos]) out[q] = Time;
-}
-
-int main(){
-    // init();
-    // push_links();
-    // traverse(s);
-    // dfs(Root);
+    cout << dp[n - 1] << '\n';
+  }
+  return 0;
 }
